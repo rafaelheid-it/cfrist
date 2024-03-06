8a4f082101485862c58d54e8408673e1a4f9a742
Rafael Heid <4heid@informatik.uni-hamburg.de>
2024-03-06 22:41:28 +0100

    [FEATURE] Add wrappers for skimage filters and test config

---
+++ config.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+# from modules.feature_removal.detectors import canny
+
+@dataclass
+class TestConfig:
+    """TestConfig for original model."""
+    content_base_directory = 'Images/test_images/content_bases'
+    style_base_directory = 'Images/test_images/style_bases'
+    embedding_directory = 'embeddings'
+    style_base_embedding_map = {
+        'axe_1.png': 'embeddings_axe_1_original_2099.pt'
+    }
+    edge_detector = None
