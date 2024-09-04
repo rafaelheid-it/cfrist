d1205a3c7357f5d551e93b1c37797358ffb6391a
Rafael Heid <4heid@informatik.uni-hamburg.de>
2024-09-04 15:56:28 +0200

    [TASK] Add example test

--- config/test/current.py
+++ config/test/current.py
@@ -1,26 +1,21 @@
-"""
-TODO: TESTS FOR EXPERIMENT "SD1.4 boots, ..."
-"""
-
 from config.test.config import TestConfig
 from modules.feature_removal.detectors import canny, difference_of_gaussians, hessian, laplace
 
-from typing import Union
 
 class Tests:
+    """Collection of TestConfigs that will be sequentially executed by the inference script."""
     test_configs: "list[TestConfig]" = []
-    """Tests for subtraction from input."""
+    
     def __init__(self) -> None:
         content_base_directory = 'Images/test_images/content_bases'
         style_base_directory = 'Images/test_images/style_bases'
-        embedding_directory = 'embeddings/fixed_feature_removal_training/3099'
+        embedding_directory = 'embeddings/'
 
-        ### STAVE TESTS
-        base_test_sd15_stave = TestConfig(
-            test_name='stave_11_input_laplace_default_sd15_3099_only_control', # override this always
-            style_image='stave_11.png',
+        base_test_staff = TestConfig(
+            test_name='staff_1_laplace_3099', # override this always
+            style_image='staff_1.png',
             content_images=['staff_cc_1.jpg'],
-            embedding='embeddings_stave_11_laplace_default_sd15_3099.pt', # override this (always?)
+            embedding='embeddings_staff_1_laplace_3099.pt',
             feature_detector={'method': laplace, 'args': {}},
             content_base_directory=content_base_directory,
             style_base_directory=style_base_directory,
@@ -30,11 +25,16 @@ class Tests:
             sd_checkpoint='models/sd/control_sd15_canny.pth',
             model_config='configs/stable-diffusion/v1-controlled-inference.yaml',
             controlled=True,
-            control_only=False, # Generate image only based on Canny edge map and disregard other content image information.
             prompt='*'
         )
 
-        self.add_test(base_test_sd15_stave)
+        self.add_test(base_test_staff)
+
+        self.add_test(base_test_staff.copy().override({
+            'test_name': 'staff_1_canny_3099',
+            'embedding': 'embeddings_staff_1_canny_3099.pt',
+            'feature_detector': {'method': canny, 'args': {}},
+        }))
 
     
     def add_test(self, test_config: TestConfig):
